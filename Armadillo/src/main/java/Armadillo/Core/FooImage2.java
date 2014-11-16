90d11d1d881aae3aae002a0f4e2dce03dde80b3f
armadilloDeveloper <armadilloDeveloper@gmail.com>
2014-11-16 15:11:15 +0100

    test

---
+++ Armadillo/src/main/java/Armadillo/Core/FooImage2.java
@@ -0,0 +1,15 @@
+package Armadillo.Core;
+
+import Armadillo.Core.UI.ImageWrapper;
+
+public class FooImage2 extends Foo
+{
+	public ImageWrapper Image;
+	
+	@Override
+	public String toString() 
+	{
+		String str = super.toString() + "_" + m_j + "_" +  m_d;
+		return str;
+	}
+}
