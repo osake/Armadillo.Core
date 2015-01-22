8e6c06406c77e0e003c03bbe79e94301dcdd2380
armadilloDeveloper <armadilloDeveloper@gmail.com>
2015-01-22 18:40:20 +0100

    test

---
+++ Armadillo/src/main/java/Armadillo/Core/UI/PublishLogItem.java
@@ -0,0 +1,14 @@
+package Armadillo.Core.UI;
+
+public class PublishLogItem {
+	
+	public String Log;
+	
+	public PublishLogItem()
+	{}
+	
+	public PublishLogItem(String strItem)
+	{
+		Log = strItem;
+	}
+}
